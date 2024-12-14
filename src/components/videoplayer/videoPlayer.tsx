c80979c3b9aa329c8e29ff347089b7fbac16766e
rafnas-p <rafnasrooni@gmail.com>
2024-12-15 00:49:21 +0530

    style

--- src/components/videoplayer/videoPlayer.tsx
+++ src/components/videoplayer/videoPlayer.tsx
@@ -43,12 +43,41 @@ const VideoPlayer: React.FC = () => {
   const [comments, setComments] = useState<CommentSnippet[]>([]);
   const [newComment, setNewComment] = useState<string>("");
   const [liked, setLiked] = useState<boolean>(false);
+  const [channels, setChannels] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   const { user } = UserAuth();
 
   if (!context) {
     throw new Error("MyContext must be used within a provider");
   }
+
+  useEffect(() => {
+    const fetchChannels = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "http://localhost:5000/api/getchannel",
+          {
+            params: { ownerId: user?.uid },
+          }
+        );
+        setChannels(response.data);
+      } catch (err: any) {
+        setError(err.response?.data?.message || "Failed to fetch channels.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (user?.uid) {
+      fetchChannels();
+    }
+  }, [user?.uid]);
+
+  console.log("channels", channels);
+
   useEffect(() => {
     const fetchVideoById = async () => {
       if (!currentVideoId) return;
@@ -137,88 +166,100 @@ const VideoPlayer: React.FC = () => {
   return (
     <div className="flex flex-col lg:flex-row px-4 mt-20 ml-14 bg-white text-gray-800 min-h-screen space-y-4 lg:space-y-0 lg:space-x-6">
       <div className="w-full lg:w-2/3 max-w-3xl space-y-4">
-        <div className="w-full bg-black rounded-xl overflow-hidden shadow-md">
-          <video
-            className="w-full h-[400px] object-cover"
-            src={videoDetails.videoUrl}
-            title="Video Player"
-            controls
-          ></video>
-        </div>
-
-        <div className="flex iteflex-col ">
-          <h1 className="text-lg font-bold">{videoDetails.title}</h1>
-          <p className="text-sm">{videoDetails.description}</p>
-        </div>
-        <div className="flex items-center space-x-4 p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200">
-          <Link
-            href={`/userAcount?username=${videoDetails.userName}`}
-            className="flex items-start space-x-4 hover:bg-gray-100 rounded-lg transition-colors duration-200"
-          >
-            <img
-              src={user?.photoURL || ""}
-              alt="Profile"
-              className="w-8 h-8 rounded-full object-cover cursor-pointer"
-            />
-            <h4>{user?.displayName}</h4>
-          </Link>
-         <button className="bg-black text-white rounded-full text-sm p-1 ">
-           subscrib
-         </button>
-          <div className="flex items-center border focus:ring-2 shadow-md bg-white rounded-md overflow-hidden cursor-pointer">
-            <div className="p-3 rounded-r-none border-r bg-gray-100 flex-1 flex justify-center items-center cursor-pointer">
-              <AiOutlineLike />
+        {channels.map((items, index) => (
+          <div key={items._id || index}>
+            <div className="w-full bg-black rounded-xl overflow-hidden shadow-md">
+              <video
+                className="w-full h-[400px] object-cover"
+                src={videoDetails.videoUrl}
+                title="Video Player"
+                controls
+              ></video>
             </div>
-
-            <div className="p-3 rounded-l-none bg-gray-100 flex-1 flex justify-center items-center">
-              <AiOutlineDislike />
+            <div className="flex iteflex-col ">
+              <h1 className="text-lg font-bold">{videoDetails.title}</h1>
+              <p className="text-sm">{videoDetails.description}</p>
             </div>
-          </div>
-        </div>
-
-        <div className="mt-6">
-          <h2 className="text-lg font-semibold">Comments</h2>
-          <div className="flex items-center space-x-2 mt-3">
-            <img
-              src={videoDetails?.profil || ""}
-              alt="User Profile"
-              className="w-10 h-10 rounded-full object-cover"
-            />
-
-            <input
-              type="text"
-              placeholder="Write a comment..."
-              value={newComment}
-              onChange={(e) => setNewComment(e.target.value)}
-              className="flex-1 border-b border-gray-300 focus:border-black px-3 py-2 text-sm outline-none transition"
-            />
-
-            <button
-              onClick={postComment}
-              className="text-black font-medium hover:bg-blue-50 px-4 py-1 rounded transition"
-            >
-              Post
-            </button>
-          </div>
-          <div className="mt-5 space-y-4">
-            {comments.map((comment) => (
-              <div key={comment._id} className="flex items-start space-x-3">
+            <div className="flex items-center space-x-4 p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200">
+              <Link
+                href={`/userAcount?username=${videoDetails.userName}`}
+                className="flex items-center space-x-4"
+              >
                 <img
-                  src={comment?.userProfile || ""}
-                  alt={comment.userName}
-                  className="w-8 h-8 rounded-full object-cover"
+                  src={items?.photoURL || ""}
+                  alt="Profile"
+                  className="w-8 h-8 rounded-full object-cover cursor-pointer"
                 />
-                <div>
-                  <p className="text-sm font-semibold">{comment.userName}</p>
-                  <p className="text-sm">{comment.text}</p>
-                  <p className="text-xs text-gray-500">
-                    {getRelativeTime(comment.createdAt)}
+
+                <div className="flex flex-col">
+                  <h4 className="font-semibold">{items?.name}</h4>
+                  <p className="text-sm text-gray-500">
+                    {items?.totalSubscribers} subscribers
                   </p>
                 </div>
+              </Link>
+
+              <button className="bg-black text-white rounded-full text-sm p-1">
+                Subscribe
+              </button>
+
+              <div className="flex items-center border focus:ring-2 shadow-md bg-white rounded-md overflow-hidden cursor-pointer">
+                <div className="p-3 rounded-r-none border-r bg-gray-100 flex-1 flex justify-center items-center cursor-pointer">
+                  <AiOutlineLike />
+                </div>
+                <div className="p-3 rounded-l-none bg-gray-100 flex-1 flex justify-center items-center">
+                  <AiOutlineDislike />
+                </div>
               </div>
-            ))}
+            </div>
+
+            <div className="mt-6">
+              <h2 className="text-lg font-semibold">Comments</h2>
+              <div className="flex items-center space-x-2 mt-3">
+                <img
+                  src={videoDetails?.profil || ""}
+                  alt="User Profile"
+                  className="w-10 h-10 rounded-full object-cover"
+                />
+
+                <input
+                  type="text"
+                  placeholder="Write a comment..."
+                  value={newComment}
+                  onChange={(e) => setNewComment(e.target.value)}
+                  className="flex-1 border-b border-gray-300 focus:border-black px-3 py-2 text-sm outline-none transition"
+                />
+
+                <button
+                  onClick={postComment}
+                  className="text-black font-medium hover:bg-blue-50 px-4 py-1 rounded transition"
+                >
+                  Post
+                </button>
+              </div>
+              <div className="mt-5 space-y-4">
+                {comments.map((comment) => (
+                  <div key={comment._id} className="flex items-start space-x-3">
+                    <img
+                      src={comment?.userProfile || ""}
+                      alt={comment.userName}
+                      className="w-8 h-8 rounded-full object-cover"
+                    />
+                    <div>
+                      <p className="text-sm font-semibold">
+                        {comment.userName}
+                      </p>
+                      <p className="text-sm">{comment.text}</p>
+                      <p className="text-xs text-gray-500">
+                        {getRelativeTime(comment.createdAt)}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
 
       <div className="w-full lg:w-1/3 pr-11">
