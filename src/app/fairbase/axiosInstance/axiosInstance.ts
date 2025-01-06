75801e0940526929c245b82520e7ab74d6da5701
rafnas-p <rafnasrooni@gmail.com>
2025-01-06 12:47:54 +0530

    firbase

--- src/app/fairbase/axiosInstance/axiosInstance.ts
+++ src/app/fairbase/axiosInstance/axiosInstance.ts
@@ -2,21 +2,20 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000", // Replace with your backend base URL
+  baseURL: "http://localhost:5000",
 });
 
-// Add a request interceptor to include the token and MongoDB _id in every request
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = Cookies.get("token"); // Retrieve the JWT token from cookies
-    const mongoDbId = Cookies.get("mongoDbId"); // Retrieve the MongoDB _id from cookies
+    const token = Cookies.get("token"); 
+    const mongoDbId = Cookies.get("mongoDbId"); 
    
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`; // Add JWT token as Authorization header
+      config.headers.Authorization = `Bearer ${token}`; 
     }
 
     if (mongoDbId) {
-      config.headers["X-MongoDb-Id"] = mongoDbId; // Add MongoDB _id as a custom header
+      config.headers["X-MongoDb-Id"] = mongoDbId; 
     }
 
     return config;
--- src/app/fairbase/config.tsx
+++ src/app/fairbase/config.tsx
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp,getApp,getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
@@ -17,6 +16,6 @@ const firebaseConfig = {
 const app =!getApps().length? initializeApp(firebaseConfig) :getApp()
 
 export const auth=getAuth(app)
-export const db = getFirestore(app); // Export Firestore instance
+export const db = getFirestore(app); 
 
 
