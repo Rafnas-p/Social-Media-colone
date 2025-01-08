26107e62f29b52db2f3b4c7c3c1ce7cea36869f8
rafnas-p <rafnasrooni@gmail.com>
2025-01-08 16:53:59 +0530

    suspense

--- src/app/(root)/userAcount/page.tsx
+++ src/app/(root)/userAcount/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Suspense } from 'react';
 import Userprofile from '@/components/userAcount/userprofile';
 function Page() {
   return (
     <div>
       
+     <Suspense fallback={<div>Loading User Shorts...</div>}>
        
         <Userprofile/>
-       
+       </Suspense>
        
     </div>
   )
